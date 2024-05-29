496de89e38475881247528be250c55890f172967
JiWoo-Yoo <selfnum_1357@naver.com>
2024-05-29 12:39:25 +0900

    [Feat] modify github finder

---
+++ github-finder/app.js
@@ -0,0 +1,27 @@
+// init github
+const Github = require('./github');
+const github = new Github();
+
+// init UI
+const ui = new UI;
+
+// Search input
+const searchUser = document.getElementById('searchUser');
+
+// search input event listener
+searchUser.addEventListener('keyup', (e) => {
+    const userText = e.target.value;
+
+    if(userText != '') {
+        github.getUser(userText).then(data => {
+            if(data.profile.message === 'Not Found') {
+                ui.showAlert('User not found', 'alert alert-danger');
+            } else {
+                ui.showProfile(data.profile);
+                ui.showRepos(data.repos);
+            }
+        })
+    } else {
+        ui.clearProfile();
+    }
+});
